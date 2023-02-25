1286d019430d477d7bec289e47cfb4f3e01d558c
Lasten-Ernest <lastenernest@gmail.com>
2023-02-25 09:26:12 +0200

    implement remove functionality

--- modules/removeActivity.js
+++ modules/removeActivity.js
@@ -1,3 +1,15 @@
 import { localStorage, getLocalStorageactivities } from "./localStorage.js";
 
-const removeActivity = (activityIndex)
+const removeActivity = (activityIndex) => {
+    const activityInStore = getLocalStorage();
+    const currntActivities = activityInStore.filter(
+        (index) => activityInStore.indexOf(index) !== activityIndex,
+    );
+    for (let i = 0; i<currntActivities.length;){
+    i = i+1;
+    currntActivities[i].index = currntActivities.indexOf(currntActivities[i])+1;
+}
+localStorage(currntActivities);
+}
+
+export default removeActivity;
